5fdd8c32e48968970be40a49529ebab78a2a27c3
Paul Ngo <inthewaves@pm.me>
2022-05-10 12:21:23 -0700

    add FusedLocationProvider (Google Play) for location
    (cherry picked from commit 4d7c62745f47f30bc32034ebdaec370f9366e9ae)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -170,6 +170,10 @@ dependencies {
     implementation(libs.google.tink)
     implementation(libs.signal.argon2)
 
+    implementation(libs.gms.location)
+
+    implementation("com.google.android.play:app-update-ktx:2.0.0")
+
     testImplementation(libs.junit.android)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
