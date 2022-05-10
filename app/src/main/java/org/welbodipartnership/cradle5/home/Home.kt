72c1db75d770d4c1c0ee4d78f58fd9aae34943a3
Paul Ngo <inthewaves@pm.me>
2022-05-09 23:06:02 -0700

    use blood icon for CRADLE tab

--- app/src/main/java/org/welbodipartnership/cradle5/home/Home.kt
+++ app/src/main/java/org/welbodipartnership/cradle5/home/Home.kt
@@ -43,6 +43,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bloodtype
 import androidx.compose.material.icons.filled.Groups
 import androidx.compose.material.icons.filled.LocationCity
 import androidx.compose.material.icons.filled.LocationOn
@@ -50,6 +51,7 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.OpenInNew
 import androidx.compose.material.icons.filled.Sync
+import androidx.compose.material.icons.outlined.Bloodtype
 import androidx.compose.material.icons.outlined.Groups
 import androidx.compose.material.icons.outlined.LocationCity
 import androidx.compose.material.icons.outlined.LocationOn
@@ -427,8 +429,8 @@ private val HomeNavigationItems = listOf(
     screen = Screen.Patients,
     labelResId = R.string.cradle_form_nav_button,
     contentDescriptionResId = R.string.cd_cradle_form_nav_button,
-    iconImageVector = Icons.Outlined.Groups,
-    selectedImageVector = Icons.Default.Groups,
+    iconImageVector = Icons.Outlined.Bloodtype,
+    selectedImageVector = Icons.Default.Bloodtype,
   ),
   HomeNavigationItem.ImageVectorIcon(
     screen = Screen.Sync,
