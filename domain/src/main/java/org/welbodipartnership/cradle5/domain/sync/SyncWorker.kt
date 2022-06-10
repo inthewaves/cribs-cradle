7e11ecb8db456c7714faead95a79569f390082c9
Paul Ngo <inthewaves@pm.me>
2022-06-10 15:05:11 -0700

    report sync errors via ACRA

--- domain/src/main/java/org/welbodipartnership/cradle5/domain/sync/SyncWorker.kt
+++ domain/src/main/java/org/welbodipartnership/cradle5/domain/sync/SyncWorker.kt
@@ -14,26 +14,33 @@ import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+import org.acra.ktx.sendSilentlyWithAcra
 import org.welbodipartnership.cradle5.data.database.CradleDatabaseWrapper
 import org.welbodipartnership.cradle5.data.database.entities.FacilityBpInfo
 import org.welbodipartnership.cradle5.data.database.entities.LocationCheckIn
 import org.welbodipartnership.cradle5.data.database.resultentities.PatientOutcomePair
 import org.welbodipartnership.cradle5.data.settings.AppValuesStore
+import org.welbodipartnership.cradle5.domain.DefaultNetworkResult
 import org.welbodipartnership.cradle5.domain.RestApi
 import org.welbodipartnership.cradle5.domain.auth.AuthRepository
 import org.welbodipartnership.cradle5.domain.facilities.BpInfoManager
 import org.welbodipartnership.cradle5.domain.patients.PatientsManager
 import org.welbodipartnership.cradle5.domain.toApiBody
+import org.welbodipartnership.cradle5.util.ApplicationCoroutineScope
 import java.security.SecureRandom
 import javax.annotation.concurrent.Immutable
 import kotlin.random.asKotlinRandom
 import kotlin.random.nextLong
+import kotlin.time.Duration.Companion.seconds
 
 @HiltWorker
 class SyncWorker @AssistedInject constructor(
@@ -45,6 +52,7 @@ class SyncWorker @AssistedInject constructor(
   private val dbWrapper: CradleDatabaseWrapper,
   private val restApi: RestApi,
   private val authRepository: AuthRepository,
+  @ApplicationCoroutineScope private val appCoroutineScope: CoroutineScope,
 ) : CoroutineWorker(appContext, workerParams) {
 
   private val secureRandom = SecureRandom()
@@ -56,67 +64,184 @@ class SyncWorker @AssistedInject constructor(
     delay(delayMillis)
   }
 
+  data class FailureInfo(
+    val formName: String,
+    val errorMessage: String
+  ) {
+    companion object {
+      fun forLocationCheckInResult(
+        checkIn: LocationCheckIn,
+        result: DefaultNetworkResult<*>
+      ) = FailureInfo(
+        formName = "LocationCheckIn",
+        "Failed to upload location checkin ${checkIn.id} " +
+          "from timestamp ${checkIn.timestamp}: " +
+          result.toString()
+      )
+
+      fun forPatientManagerResult(
+        pair: PatientOutcomePair,
+        result: PatientsManager.UploadResult
+      ): FailureInfo {
+        val message = buildString {
+          val error: RestApi.PostResult?
+          when (result) {
+            PatientsManager.UploadResult.NoOutcomesObjectIdFailure -> {
+              append("NoOutcomesObjectIdFailure")
+              error = null
+            }
+            PatientsManager.UploadResult.NoPatientObjectIdFailure -> {
+              append("NoPatientObjectIdFailure")
+              error = null
+            }
+            is PatientsManager.UploadResult.OutcomesFailure -> {
+              append("OutcomesFailure")
+              error = result.error
+            }
+            is PatientsManager.UploadResult.PatientFailure -> {
+              append("PatientFailure")
+              error = result.error
+            }
+            PatientsManager.UploadResult.Success -> {
+              append("Success(?)")
+              error = null
+            }
+          }
+          append(" for patient id ${pair.patient?.id} ")
+          append("(server info: ${pair.patient?.serverInfo}) and ")
+          append("outcomes id ${pair.outcomes?.id} ")
+          append("(server info: ${pair.outcomes?.serverInfo}); error: ")
+          append(error.toString())
+        }
+        return FailureInfo(formName = "Patient/Outcomes", message)
+      }
+
+      fun forBloodPressureManagerResult(
+        bpInfo: FacilityBpInfo,
+        result: BpInfoManager.UploadResult
+      ): FailureInfo {
+        val message = buildString {
+          val error: RestApi.PostResult? = when (result) {
+            is BpInfoManager.UploadResult.NoMetaInfoFailure -> {
+              append("NoMetaInfoFailure")
+              null
+            }
+            is BpInfoManager.UploadResult.Failure -> {
+              append("Failure")
+              result.error
+            }
+            BpInfoManager.UploadResult.Success -> {
+              append("Success(?)")
+              null
+            }
+          }
+          append(" for blood pressure info id ${bpInfo.id} ")
+          append("(server info: ${bpInfo.serverInfo}) and ")
+          append(error.toString())
+        }
+        return FailureInfo(formName = "FacilityBpInfo", message)
+      }
+    }
+  }
+
   override suspend fun doWork(): Result {
     Log.d(TAG, "starting SyncWorker")
     reportProgress(Stage.STARTING)
 
-    Log.d(TAG, "uploading patients")
-    val newPatientsUploadResult = runUploadForPatientsAndOutcomes(
-      Stage.UPLOADING_NEW_PATIENTS,
-      dbWrapper
-        .patientsDao()
-        .getNewPatientsToUploadOrderedById()
-    )
-
-    Log.d(TAG, "uploading any failed patients")
-    val failedPatientsUploadResult = runUploadForPatientsAndOutcomes(
-      Stage.UPLOADING_INCOMPLETE_PATIENTS,
-      dbWrapper
-        .patientsDao()
-        .getPatientsWithPartialServerInfoOrderedById()
-    )
-
-    Log.d(TAG, "uploading any patients with unuploaded outcomes")
-    val failedOutcomesResult = runUploadForPatientsAndOutcomes(
-      Stage.UPLOADING_INCOMPLETE_OUTCOMES,
-      dbWrapper
-        .outcomesDao()
-        .getOutcomesNotFullyUploadedOrderedWithOrWithoutErrorsById()
-    )
-
-    Log.d(TAG, "uploading check ins")
-    runUploadForCheckIns(dbWrapper.locationCheckInDao().getCheckInsForUpload())
-
-    Log.d(TAG, "uploading bp info")
-    runUploadForBpInfoForms(
-      Stage.UPLOADING_BP_INFO, dbWrapper.bpInfoDao().getNewFormsToUploadOrderedById()
-    )
-
-    Log.d(TAG, "uploading incomplete bp info")
-    runUploadForBpInfoForms(
-      Stage.UPLOADING_INCOMPLETE_BP_INFO,
-      dbWrapper.bpInfoDao().getFormsWithPartialServerInfoOrderedById()
-    )
-
+    val errors = mutableListOf<FailureInfo>()
     coroutineScope {
-      val progressChannel = actor<AuthRepository.InfoSyncProgress>(capacity = Channel.CONFLATED) {
+      val errorsActor = actor<FailureInfo> {
         consumeEach {
-          Log.d(TAG, "${it.stage}: ${it.text}")
-          reportInfoProgress(it.stage, it.text)
+          errors.add(it)
         }
       }
-      try {
-        authRepository.doLoginInfoSync(
-          AuthRepository.InfoSyncProgressReceiver.StageAndStringMessages(progressChannel)
-        )
-      } finally {
-        progressChannel.close()
+
+      Log.d(TAG, "uploading patients")
+      val newPatientsUploadResult = runUploadForPatientsAndOutcomes(
+        Stage.UPLOADING_NEW_PATIENTS,
+        dbWrapper
+          .patientsDao()
+          .getNewPatientsToUploadOrderedById(),
+        errorsActor
+      )
+
+      Log.d(TAG, "uploading any failed patients")
+      val failedPatientsUploadResult = runUploadForPatientsAndOutcomes(
+        Stage.UPLOADING_INCOMPLETE_PATIENTS,
+        dbWrapper
+          .patientsDao()
+          .getPatientsWithPartialServerInfoOrderedById(),
+        errorsActor
+      )
+
+      Log.d(TAG, "uploading any patients with unuploaded outcomes")
+      val failedOutcomesResult = runUploadForPatientsAndOutcomes(
+        Stage.UPLOADING_INCOMPLETE_OUTCOMES,
+        dbWrapper
+          .outcomesDao()
+          .getOutcomesNotFullyUploadedOrderedWithOrWithoutErrorsById(),
+        errorsActor
+      )
+
+      Log.d(TAG, "uploading check ins")
+      runUploadForCheckIns(dbWrapper.locationCheckInDao().getCheckInsForUpload(), errorsActor)
+
+      Log.d(TAG, "uploading bp info")
+      val newBpUploadResult = runUploadForBpInfoForms(
+        Stage.UPLOADING_BP_INFO,
+        dbWrapper.bpInfoDao().getNewFormsToUploadOrderedById(),
+        errorsActor,
+      )
+
+      Log.d(TAG, "uploading incomplete bp info")
+      runUploadForBpInfoForms(
+        Stage.UPLOADING_INCOMPLETE_BP_INFO,
+        dbWrapper.bpInfoDao()
+          .getFormsWithPartialServerInfoOrderedById()
+          .filter { it.id !in newBpUploadResult.failedIds },
+        errorsActor,
+      )
+
+      coroutineScope {
+        val progressChannel = actor<AuthRepository.InfoSyncProgress>(capacity = Channel.CONFLATED) {
+          consumeEach {
+            Log.d(TAG, "${it.stage}: ${it.text}")
+            reportInfoProgress(it.stage, it.text)
+          }
+        }
+        try {
+          authRepository.doLoginInfoSync(
+            AuthRepository.InfoSyncProgressReceiver.StageAndStringMessages(progressChannel)
+          )
+        } finally {
+          progressChannel.close()
+        }
+      }
+
+      errorsActor.close()
+    }
+
+    if (errors.isNotEmpty()) {
+      Log.w(TAG, "encountered errors; launching delayed coroutine to report them")
+      // FIXME: Figure out why ACRA is cancelling our workers.
+      appCoroutineScope.launch {
+        delay(1.seconds)
+        Log.w(TAG, "reporting errors to ACRA")
+        val errorMessage = buildString {
+          errors.forEach { (formName, message) ->
+            append("Failure for form $formName: ")
+            appendLine(message)
+          }
+        }.trimEnd('\n')
+        SyncException(errorMessage).sendSilentlyWithAcra()
       }
     }
 
     appValuesStore.setLastTimeSyncCompletedToNow()
     return Result.success()
   }
+
+  class SyncException(override val message: String) : Exception(message)
 
   private data class UploadResult(
     val successfulIds: Set<Long>,
@@ -125,7 +250,8 @@ class SyncWorker @AssistedInject constructor(
 
   private suspend fun runUploadForPatientsAndOutcomes(
     stage: Stage,
-    patientsAndOutcomes: List<PatientOutcomePair>
+    patientsAndOutcomes: List<PatientOutcomePair>,
+    errorsChannel: SendChannel<FailureInfo>
   ): UploadResult = coroutineScope {
     val successfulPatientIds = linkedSetOf<Long>()
     val failedPatientIds = linkedSetOf<Long>()
@@ -150,13 +276,14 @@ class SyncWorker @AssistedInject constructor(
       if (patient == null) {
         Log.w(TAG, "refusing to upload outcome ${outcomes?.id} because missing patient")
       } else if (outcomes == null) {
-        Log.w(TAG, "refusing to upload patient ${patient?.id} because they have no outcomes")
+        Log.w(TAG, "refusing to upload patient ${patient.id} because they have no outcomes")
       } else {
         val result = patientsManager.uploadPatientAndOutcomes(patient, outcomes)
         Log.d(TAG, "patient result: $result")
         if (result is PatientsManager.UploadResult.Success) {
           successfulPatientIds.add(patient.id)
         } else {
+          errorsChannel.send(FailureInfo.forPatientManagerResult(patientOutcomePair, result))
           failedPatientIds.add(patient.id)
           Log.d(TAG, "got a failed result; applying retry jitter")
           delayWithRetryJitter()
@@ -169,7 +296,10 @@ class SyncWorker @AssistedInject constructor(
     UploadResult(successfulPatientIds, failedPatientIds)
   }
 
-  private suspend fun runUploadForCheckIns(checkIns: List<LocationCheckIn>): Unit = coroutineScope {
+  private suspend fun runUploadForCheckIns(
+    checkIns: List<LocationCheckIn>,
+    errorsChannel: SendChannel<FailureInfo>,
+  ): Unit = coroutineScope {
     reportProgress(
       Stage.UPLOADING_LOCATION_CHECK_INS,
       doneSoFar = 0,
@@ -202,6 +332,7 @@ class SyncWorker @AssistedInject constructor(
           if (result.isSuccess) {
             dbWrapper.locationCheckInDao().markAsUploaded(checkInId = checkIn.id)
           } else {
+            errorsChannel.send(FailureInfo.forLocationCheckInResult(checkIn, result))
             failedCheckInIds.add(checkIn.id)
             Log.d(TAG, "got a failed result; applying retry jitter")
             delayWithRetryJitter()
@@ -216,7 +347,8 @@ class SyncWorker @AssistedInject constructor(
 
   private suspend fun runUploadForBpInfoForms(
     stage: Stage,
-    forms: List<FacilityBpInfo>
+    forms: List<FacilityBpInfo>,
+    errorsChannel: SendChannel<FailureInfo>,
   ): UploadResult = coroutineScope {
     val successfulIds = linkedSetOf<Long>()
     val failedIds = linkedSetOf<Long>()
@@ -241,6 +373,7 @@ class SyncWorker @AssistedInject constructor(
       if (result is BpInfoManager.UploadResult.Success) {
         successfulIds.add(form.id)
       } else {
+        errorsChannel.send(FailureInfo.forBloodPressureManagerResult(form, result))
         failedIds.add(form.id)
         Log.d(TAG, "got a failed result; applying retry jitter")
         delayWithRetryJitter()
