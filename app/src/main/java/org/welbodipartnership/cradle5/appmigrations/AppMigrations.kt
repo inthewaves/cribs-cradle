b80a27bd7ae0fefed6a33be84cbf3fef4a4ece2d
Paul Ngo <inthewaves@pm.me>
2022-07-12 13:35:22 -0700

    add app migration for syncing the OTHER facility

--- app/src/main/java/org/welbodipartnership/cradle5/appmigrations/AppMigrations.kt
+++ app/src/main/java/org/welbodipartnership/cradle5/appmigrations/AppMigrations.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.withContext
 import org.welbodipartnership.cradle5.data.cryptography.ArgonHasher
 import org.welbodipartnership.cradle5.data.settings.AppValuesStore
+import org.welbodipartnership.cradle5.domain.sync.SyncRepository
 import org.welbodipartnership.cradle5.util.coroutines.AppCoroutineDispatchers
 import javax.inject.Inject
 
@@ -13,15 +14,17 @@ class AppMigrations @Inject internal constructor(
   private val appValuesStore: AppValuesStore,
   private val hasher: ArgonHasher,
   private val appCoroutineDispatchers: AppCoroutineDispatchers,
+  private val syncRepository: SyncRepository,
 ) {
   companion object {
     private const val TAG = "AppMigrations"
 
-    const val CURRENT_VERSION = 1
+    const val CURRENT_VERSION = 2
   }
 
   private object Version {
     const val USERNAME_HASH = 1
+    const val OTHER_FACILITY_SYNC = 2
   }
 
   suspend fun runMigrations(application: Application) {
@@ -41,6 +44,9 @@ class AppMigrations @Inject internal constructor(
           }
         )
       }
+    }
+    if (lastSeenVersion < Version.OTHER_FACILITY_SYNC) {
+      syncRepository.enqueueDownloadSyncJob()
     }
 
     appValuesStore.setLastAppMigrationVersion(CURRENT_VERSION)
